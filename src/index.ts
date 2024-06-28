b59f431be5ea255b19cac74f79e1970ec3d31059
Prem <premkumar@divami.com>
2024-06-28 19:09:00 +0530

    Added express and basic entry middlewares

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
+import { app } from "./app";
 import { connectDB } from "./db";
 require("dotenv").config({});
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`PORT connected Successfully : ${process.env.PORT || 8000}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB Connection Error", err);
+  });
